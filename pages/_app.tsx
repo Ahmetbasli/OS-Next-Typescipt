823352984a6a81f95ad062c91d7cec59fe0867e4
Ahmetbasli <ahmet.basli.mec@gmail.com>
2022-04-14 16:35:12 +0300

    add title to html

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,16 @@
 import 'styles/globals.scss';
 
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>My page title</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Component {...pageProps} />;
+    </>
+  );
 }
