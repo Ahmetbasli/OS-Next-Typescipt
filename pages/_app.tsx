2ee020f49651c9af8af8877755a7b36d7f8ee658
Ahmetbasli <ahmet.basli.mec@gmail.com>
2022-04-14 16:41:22 +0300

    Change meta description

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,12 +9,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <title>My page title</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta
-          name="description"
-          content="Author: A.N. Author,
-    Illustrator: P. Picture, Category: Books, Price: $17.99,
-    Length: 784 pages"
-        />
+        <meta name="description" content="os-nextjs-typescript project" />
       </Head>
       <Component {...pageProps} />;
     </>
