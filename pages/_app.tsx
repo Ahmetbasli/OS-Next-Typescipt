14ea6a23f144b4fe789720b1e452461f0f288999
Ahmetbasli <ahmet.basli.mec@gmail.com>
2022-04-15 21:56:59 +0300

    Add styled component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,25 @@
-import 'styles/globals.scss';
-
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+html,
+body {
+  color: red;
+  margin: 0;
+  padding: 0;
+}
+
+* {
+  box-sizing: sborder-box;
+}
+`;
+
+const theme = {
+  colors: {
+    primary: 'red',
+  },
+};
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -11,7 +29,10 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta name="description" content="os-nextjs-typescript project" />
       </Head>
-      <Component {...pageProps} />;
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Component {...pageProps} />;
+      </ThemeProvider>
     </>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,11 @@
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  color: ${({ theme }) => theme.colors.primary};
+  font-size: 1.5em;
+  text-align: center;
+`;
+
 export default function Home() {
-  return <h1>Hello, world!</h1>;
+  return <Title>Hello, world!</Title>;
 }
