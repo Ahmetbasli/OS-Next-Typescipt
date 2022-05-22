db5252ca64449ffb371cc267e2fbcaac321e13d1
Ahmetbasli <ahmet.basli.mec@gmail.com>
2022-05-22 17:24:25 +0300

    Fix jest state update bug

--- __test__/pages/index.spec.tsx
+++ __test__/pages/index.spec.tsx
@@ -1,15 +1,17 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import StyledApp from 'components/pages/StyledApp';
 import Index from 'pages/index';
 
 describe('Index', () => {
-  it('renders a heading', () => {
+  it('renders a heading', async () => {
     render(
       <StyledApp>
         <Index />
       </StyledApp>
     );
 
-    expect(screen.getByText('Hello, world!')).toBeInTheDocument();
+    await waitFor(() => {
+      expect(screen.getByText('Hello, world!')).toBeInTheDocument();
+    });
   });
 });
