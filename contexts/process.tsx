b7fc6003443165313943285c6cd9c2b7c25fb071
Ahmetbasli <ahmet.basli.mec@gmail.com>
2022-05-23 18:47:23 +0300

    Extracted initial context states

--- contexts/process.tsx
+++ contexts/process.tsx
@@ -2,10 +2,11 @@ import useProcessContextState from 'hooks/useProcessContextState';
 import type { FC } from 'react';
 import React, { createContext } from 'react';
 import type { ProcessContextState } from 'types/contexts/Process';
+import { initialContextProcessesState } from 'utils/initialContextstates';
 
-export const ProcessContext = createContext<ProcessContextState>({
-  processes: {},
-});
+export const ProcessContext = createContext<ProcessContextState>(
+  initialContextProcessesState
+);
 
 export const ProcessProvider: FC<{ children: React.ReactNode }> = ({
   children,
